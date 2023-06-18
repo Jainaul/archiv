48dc4991096d5b5d828af520dff929bd9cb1149e
Jun <rcqed@outlook.com>
2023-06-18 14:25:25 +0800

    1.1

--- config/1-01_creatsheet.py
+++ config/1-01_creatsheet.py
@@ -11,8 +11,8 @@ def write_folder_names_to_excel(directory, excel_file):
     sheet = workbook.active
     sheet.title = "Sheet1"  # 设置第一个 sheet 的名称为 "Sheet1"
     
-    # 获取指定目录下的所有文件夹名称
-    folder_names = [name for name in os.listdir(directory) if os.path.isdir(os.path.join(directory, name))]
+    # 获取指定目录下的所有文件夹名称，忽略"date.txt"
+    folder_names = [name for name in os.listdir(directory) if os.path.isdir(os.path.join(directory, name)) and name != "date.txt"]
     
     # 将文件夹名称写入 Excel 表格的第一列并分列
     for index, folder_name in enumerate(folder_names, start=1):
@@ -34,14 +34,15 @@ def write_file_names_to_excel(directory, excel_file):
     # 创建一个新的 Sheet，命名为 "Sheet2"
     sheet2 = workbook.create_sheet("Sheet2")
 
-    # 递归获取指定目录下的所有文件的文件名
+    # 递归获取指定目录下的所有文件的文件名，忽略"date.txt"
     file_names = []
     for root, dirs, files in os.walk(directory):
         for file in files:
             file_path = os.path.join(root, file)
-            file_name = os.path.splitext(os.path.basename(file_path))[0]
-            file_names.append(file_name)
-            print(f"正在处理文件: {file_name}")  # 显示当前正在处理的文件
+            if file != "date.txt":
+                file_name = os.path.splitext(os.path.basename(file_path))[0]
+                file_names.append(file_name)
+                print(f"正在处理文件: {file_name}")  # 显示当前正在处理的文件
     
     # 将文件名写入 Sheet2 的第一列并分列
     for index, file_name in enumerate(file_names, start=1):
